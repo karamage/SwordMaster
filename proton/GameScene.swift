fcc2f668b0ed3cf40961eab309747111ddfdec8d
karamage <karamage.umauma@gmail.com>
2015-07-09 17:34:37 +0900

    アニメーション処理を追加した

--- proton/GameScene.swift
+++ proton/GameScene.swift
@@ -19,6 +19,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         static let Enegy: UInt32 = (1<<5)
         static let None: UInt32 = (1<<9)
     }
+    //ラベル
+    let gameoverLabel = SKLabelNode(fontNamed:"Hiragino Kaku Gothic ProN")
+    let scoreLabel = SKLabelNode(fontNamed:"Hiragino Kaku Gothic ProN")
+    
+    //スコア
+    var score: UInt = 0
+
     //ゲームオーバーフラグ
     var gameoverflg = false
     //敵作成フラグ
@@ -40,6 +47,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //プレイヤー
     var player: SKSpriteNode!
     var playerTexture = SKTexture(imageNamed: "player1")
+    var playerTexture2 = SKTexture(imageNamed: "player2")
     
     //敵配置用ノード
     var enemysNode = SKNode()
@@ -65,9 +73,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //bg.size = self.size
         makeBgParticle()
         
+        //背景を徐々に下にスクロールする
+        var bgScrollAction = SKAction.moveToY(-60.0, duration: 20)
+        var bgScrollRevAction = SKAction.moveToY(15.0, duration: 10)
+        var bgScroll = SKAction.repeatActionForever(SKAction.sequence([bgScrollAction,bgScrollRevAction]))
+        bg.runAction(bgScroll)
+        
         //重力の設定
         self.physicsWorld.gravity = CGVector(dx:0.0, dy:-0.10)
         self.physicsWorld.contactDelegate = self
+        
+        //ラベルの表示
+        scoreLabel.text = "\(score)"
+        scoreLabel.fontSize = 25
+        scoreLabel.fontColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        scoreLabel.zPosition = 1000
+        self.addChild(scoreLabel)
+        scoreLabel.position = CGPoint(x: (self.frame.size.width/2), y: self.frame.size.height - 30)
         
         //剣配置用ノード
         swordsNode.position = CGPoint(x:0, y:0)
@@ -84,9 +106,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //プレイヤーを作成
         makePlayer()
         
+        //羽のアニメーションを切り出す
+        var haneAim: [SKTexture] = explodeAnime("wing", xFrame: 5, yFrame: 6)
+        var haneAim2: [SKTexture] = [SKTexture](haneAim[7...22])
+        var hane = SKSpriteNode(texture: haneAim[0], size: haneAim[0].size())
+        self.addChild(hane)
+        hane.position = CGPoint(x: self.frame.size.width/2, y: 50)
+        
+        var haneAnimAction = SKAction.animateWithTextures(haneAim, timePerFrame: 0.1, resize:false, restore:true)
+        var haneAnimAction2 = SKAction.animateWithTextures(haneAim2, timePerFrame: 0.1, resize:false, restore:true)
+        var repeatHaneAction = SKAction.repeatActionForever(haneAnimAction2)
+        hane.runAction(SKAction.sequence([haneAnimAction,repeatHaneAction]))
+        
+        //ワープのアニメーション
+        var warpAim: [SKTexture] = explodeAnime("warp", xFrame: 2, yFrame: 13)
+        var warpAim2: [SKTexture] = [SKTexture](warpAim[0...14])
+        var warp = SKSpriteNode(texture: warpAim[0], size: warpAim[0].size())
+        self.addChild(warp)
+        warp.position = CGPoint(x: self.frame.size.width/2, y: 50)
+        var warpAnimAction = SKAction.animateWithTextures(warpAim2, timePerFrame: 0.1, resize:false, restore:true)
+        var warpRemoveAction = SKAction.removeFromParent()
+        warp.runAction(SKAction.sequence([warpAnimAction,warpRemoveAction]))
+        
         //敵を作成する
         makeEnemySample()
-        //makeEnemySample()
         
         //BGMを鳴らす
         let bgSoundRepeat = SKAction.repeatActionForever(bgSound)
@@ -124,7 +167,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //プレイヤー作成
     func makePlayer() {
         player = SKSpriteNode(texture: playerTexture)
-        player.position = CGPoint(x: self.frame.size.width * 0.5, y: 50)
+        player.position = CGPoint(x: self.frame.size.width * 0.5, y: 0)
         
         //物理シミュレーション設定
         player.physicsBody = SKPhysicsBody(texture: playerTexture, size: playerTexture.size())
@@ -136,6 +179,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.addChild(player)
         player.zPosition = 2
+        
+        //パラパラアニメーション
+        let paraAction = SKAction.animateWithTextures([playerTexture,playerTexture2], timePerFrame: 0.2)
+        let repeatParaAction = SKAction.repeatActionForever(paraAction)
+        player.runAction(repeatParaAction)
+        
+        //画面下から登場
+        var playerAction = SKAction.moveToY(50, duration: 2)
+        player.runAction(playerAction)
+        
+        //パーティクル作成
+        let particlePosition = CGPoint(x: self.frame.size.width * 0.5, y: 50)
+        makeParticle(particlePosition, filename:"playerParticle.sks", hide:false)
     }
     
     //敵1の作成
@@ -159,7 +215,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         var randY = arc4random_uniform(100)
         x = CGFloat(randX)
         y = CGFloat(self.frame.size.height - CGFloat(randY))
-        println("x:\(x) y:\(y)")
+        //println("x:\(x) y:\(y)")
         
         enemysNode.addChild(enemy1)
         enemy1.position = CGPoint(x:x,y:y)
@@ -167,6 +223,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         //プレイヤーに迫って移動してくるようにする
         var action = SKAction.moveTo(player.position, duration: 10)
         enemy1.runAction(action)
+        
+        //回転のアニメーションをランダム時間で付ける
+        var rotateAction = SKAction.rotateByAngle(CGFloat(360*M_PI/180), duration: 0.5)
+        var rotateWaitAction = SKAction.waitForDuration(NSTimeInterval(CGFloat(randY) * 0.05))
+        var rotate = SKAction.repeatActionForever(SKAction.sequence([rotateAction,rotateWaitAction]))
+        enemy1.runAction(rotate)
     }
     
     //剣の作成
@@ -215,22 +277,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             //敵に剣が当たったら消す
             if (contact.bodyA.categoryBitMask & enemyType == enemyType) {
                 contact.bodyB.categoryBitMask = ColliderType.None
-                makeColliParticle(contact.bodyA.node?.position)
-                contact.bodyA.node?.removeFromParent()
-                contact.bodyA.node?.removeAllActions()
-                killcount++
-                if killcount % 10 == 0 {
-                    makeenemyflg = true
-                }
+                killEnemy(contact.bodyA.node)
             } else if (contact.bodyB.categoryBitMask & enemyType == enemyType) {
                 contact.bodyA.categoryBitMask = ColliderType.None
-                makeColliParticle(contact.bodyB.node?.position)
-                contact.bodyB.node?.removeFromParent()
-                contact.bodyB.node?.removeAllActions()
-                killcount++
-                if killcount % 10 == 0 {
-                    makeenemyflg = true
-                }
+                killEnemy(contact.bodyB.node)
             } else if contact.bodyA.categoryBitMask & ColliderType.None == ColliderType.None ||
                 contact.bodyB.categoryBitMask & ColliderType.None == ColliderType.None {
                 //剣と剣の衝突
@@ -239,6 +289,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 self.runAction(swordSound)
             }
         }
+    }
+    
+    //敵を倒した時の処理
+    func killEnemy(enemynode: SKNode!) {
+        enemynode.physicsBody?.categoryBitMask = ColliderType.None
+        makeColliParticle(enemynode.position)
+        killcount++
+        score += 10
+        scoreLabel.text = "\(score)"
+        if killcount % 10 == 0 {
+            makeenemyflg = true
+        }
+        fadeRemoveNode(enemynode)
     }
     
     //背景用パーティクル作成
@@ -259,22 +322,74 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     //パーティクル発生
-    func makeParticle(position:CGPoint?, filename: String) {
+    func makeParticle(position:CGPoint?, filename: String, hide: Bool = true) {
         let particle = SKEmitterNode(fileNamed: filename)
         self.addChild(particle)
-        
-        //１秒後に消す
-        var removeAction = SKAction.removeFromParent()
-        var durationAction = SKAction.waitForDuration(1.50)
-        var sequenceAction = SKAction.sequence([durationAction,removeAction])
-        particle.runAction(sequenceAction)
         
         if let tmp = position {
             particle.position = tmp
         }
-        particle.alpha = 1
+        
+        //消さないんだったらリターン
+        if !hide {
+            return
+        }
+        fadeRemoveNode(particle)
+    }
+    
+    //一秒後にフェードしながらノードを消す
+    func fadeRemoveNode(removenode: SKNode!) {
+        //１秒後に消す
+        var removeAction = SKAction.removeFromParent()
+        var durationAction = SKAction.waitForDuration(1.50)
+        var sequenceAction = SKAction.sequence([durationAction,removeAction])
+        removenode.runAction(sequenceAction)
+        
+        removenode.alpha = 1
         
         var fadeAction = SKAction.fadeAlphaTo(0, duration: 1.0)
-        particle.runAction(fadeAction)
+        removenode.runAction(fadeAction)
+    }
+    
+    //一枚の画像からアニメーションを切り出す
+    func explodeAnime(imageName: String, xFrame: UInt, yFrame: UInt) -> [SKTexture] {
+        
+        var ret: [SKTexture] = []
+        var image = UIImage(named: imageName)
+        
+        var inner: CGImageRef! = image?.CGImage
+        
+        var xFrameInt: Int = Int(xFrame)
+        var yFrameInt: Int = Int(yFrame)
+        
+        //var width: Int = Int(image?.size.width) / xFrameInt
+        var width:Int = 0
+        var height:Int = 0
+        var scale: CGFloat = 0
+        if let tmpimage = image {
+            width = Int(tmpimage.size.width) / xFrameInt
+            height = Int(tmpimage.size.height) / yFrameInt
+            scale = tmpimage.scale
+        }
+        
+        var fx: CGFloat = 0
+        var fy: CGFloat = 0
+        var fwidth: CGFloat = 0
+        var fheight: CGFloat = 0
+        for i in 0..<yFrameInt {
+            for i2 in 0..<xFrameInt {
+                fx = 0+(CGFloat(i2 * width) * scale)
+                fy = 0+(CGFloat(i * height) * scale)
+                fwidth = CGFloat(width)*scale
+                fheight = CGFloat(height)*scale
+                var rect: CGRect = CGRectMake(fx, fy, fwidth, fheight)
+                var ref: CGImageRef = CGImageCreateWithImageInRect(inner, rect)
+                //var rev: UIImage? = UIImage(CGImage: ref)
+                var texture = SKTexture(CGImage: ref)
+                ret.append(texture)
+            }
+        }
+        
+        return ret
     }
 }
